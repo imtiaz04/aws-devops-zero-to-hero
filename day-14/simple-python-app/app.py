6b87f360cc7d8dfb0b9fe47f5ec77cee9d6aa0df
imtiaz04 <85178565+imtiaz04@users.noreply.github.com>
2023-07-31 14:58:54 -0400

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,8 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return 'Hello, world!!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run() 
+
+
+
+
+
+
 
